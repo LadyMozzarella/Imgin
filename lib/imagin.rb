9a140444093c85ed1728f4ca3d5f074de96c9ea8
Mason ONeal <masonwid@gmail.com>
2014-02-13 14:37:53 -0800

    Initial Push with file structure

---
+++ lib/imagin.rb
@@ -0,0 +1,9 @@
+# ImagIn program takes a word (or string) as an argument and returns a URL or an image based on an image search of that word or string
+
+# require CGI
+require 'open-uri'
+require 'nokogiri'
+
+# DRAFT - draft not final text/comments
+
+# the rest is coming! right now!
