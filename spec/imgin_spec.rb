1d0534d89f08c1aea6b5f4b606dde0708461dfe9
menor <josemenorgmail.com>
2014-02-14 11:52:50 -0800

    A little feather duster here and there

--- spec/imgin_spec.rb
+++ spec/imgin_spec.rb
@@ -1,26 +1,8 @@
-require_relative '../lib/imagin'
+require_relative '../lib/imgin'
 
-<<<<<<< HEAD
-# before
-describe 'search' do
-  it "should return a google images webpage" do
-    Imgin.search('cats') == Nokogiri::HTML(open("https://www.google.com/search?site=imghp&tbm=isch&q=cats"))
-  end
-
-end
-
-describe select_img do
-  it "should return an image file" do
-    cats = Nokogiri::HTML(open("https://www.google.com/search?site=imghp&tbm=isch&q=cats"))
-    Imgin.select_img(cats) =~ /.+\.(png|jpg|gif|svg)/
-  end
-end
-
-
-=======
 describe Image do
   let(:image) { Image.new }
-    it "should return a google images webpage" do
+    it "should return a webpage" do
       expect(image.get_search_results('cats').class).to eq(Nokogiri::HTML::Document)
     end
 
@@ -28,5 +10,6 @@ describe Image do
     cats = Nokogiri::HTML(open("https://www.google.com/search?site=imghp&tbm=isch&q=cats"))
     expect(image.select_image(cats)).to  match(/.+\.(png|jpg|gif|svg)/)
   end
+
 end
->>>>>>> master
+
